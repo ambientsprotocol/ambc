b513119ae98c64e9c2d3d69add65ed8e39903f72
haad <haad@haja.io>
2019-10-02 13:28:58 +0300

    Fix tests

--- test/fromjs.spec.js
+++ test/fromjs.spec.js
@@ -5,10 +5,10 @@ const fs = require('fs')
 const FIXTURES_PATH = 'test/fixtures/js/'
 
 describe('JS Compiler', function () {
-  it('parses ', () => {
+  it('compiles ', () => {
     const fixtures = fs.readdirSync(FIXTURES_PATH)
     while (fixtures.length > 0) {
-      console.log(`Parsing: ${fixtures[0].split('.')[0]}`)
+      console.log(`Compiling: ${fixtures[0].split('.')[0]}`)
       // console.log(JSON.stringify(parse(syntax.toString().trim())))
       const result = fs.readFileSync(FIXTURES_PATH + fixtures[0])
       assert.strictEqual(ambients.fromJS(FIXTURES_PATH + fixtures[1]), result.toString().replace(/\r?\n\s*|\r\s*/g, "").replace(/\s+/g, ' '))
