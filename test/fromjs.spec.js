ac49790adb03404627cfd66e521599d259d620c6
Mark Henderson <henderson.mark@gmail.com>
2019-10-02 10:19:06 -0400

    Test file formatting

--- test/fromjs.spec.js
+++ test/fromjs.spec.js
@@ -12,7 +12,11 @@ describe('JS Compiler', function () {
       console.log(`Compiling: ${fixtures[0].split('.')[0]}`)
       // console.log(JSON.stringify(parse(syntax.toString().trim())))
       const result = fs.readFileSync(FIXTURES_PATH + fixtures[0])
-      assert.strictEqual(ambients.fromJS(FIXTURES_PATH + fixtures[1]), result.toString().replace(/\r?\n\s*|\r\s*/g, "").replace(/\s+/g, ' '), 'Test case ' + (len - fixtures.length / 2 + 1) + " failed")
+      assert.strictEqual(
+        ambients.fromJS(FIXTURES_PATH + fixtures[1]),
+        result.toString().replace(/\r?\n\s*|\r\s*/g, '').replace(/\s+/g, ' '),
+        'Test case ' + (len - fixtures.length / 2 + 1) + ' failed'
+      )
       fixtures.splice(0, 2)
     }
   })
