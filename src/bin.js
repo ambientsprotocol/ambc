c7c1789dd516b9438876a8756e7941e4a47455a1
Mark Henderson <henderson.mark@gmail.com>
2019-10-04 10:16:18 -0400

    File re-org and updated fixtures for flex-js

--- src/bin.js
+++ src/bin.js
@@ -6,7 +6,8 @@ const mime = require('mime-types')
 const argv = require('yargs')
   .usage('$0 file [-o output]')
   .argv
-const { parser } = require('./index')
+
+const { parse, irParser } = require('./index')
 
 // Register new MIME type for .ambient files
 mime.extensions['text/ambients'] = ['ambient']
@@ -15,15 +16,20 @@ mime.types.ambient = 'text/ambients'
 const filename = argv._[0]
 let results, ambient
 
+const output = (ambient, argv) => {
+  if (argv.intermediate) return irParser.parse(ambient)
+  return parse(ambient)
+}
+
 switch (mime.lookup(filename)) {
   case 'application/javascript':
     var js = fs.readFileSync(filename).toString().trim()
     ambient = js2amb(js)
-    results = parser.parse(ambient)
+    results = output(ambient, argv)
     break
   case 'text/ambients':
     ambient = fs.readFileSync(filename).toString().trim()
-    results = parser.parse(ambient)
+    results = output(ambient, argv)
     break
   default:
     throw new Error('File type not recognized')
